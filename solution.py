fabc7fd4b8ef1457b22e3ecc177bb4dd37d1a4f6
kriti-04 <70703431+kriti-04@users.noreply.github.com>
2022-11-11 09:43:46 +0530

    Add files via upload

---
+++ solution.py
@@ -0,0 +1,19 @@
+import sys
+number_of_days = int(sys.argv[1])
+if number_of_days<4:
+    #solution when the number of days N is less than 4
+    print(str(2**(number_of_days-1))+'/'+str(2**number_of_days))
+else:
+    consecutive_days = 4 # since he/she is not allowed to miss classes for four or more consecutive days
+    probability = 2**3 
+    no_of_combo_with_one_day_absent = 2 
+    no_of_combo_with_two_day_absent = 1
+    no_of_combo_with_three_day_absent = 1
+    for iteration in range(4,number_of_days+1):
+        i = no_of_combo_with_three_day_absent
+        no_of_combo_with_three_day_absent = no_of_combo_with_two_day_absent
+        no_of_combo_with_two_day_absent = no_of_combo_with_one_day_absent
+        no_of_combo_with_one_day_absent = consecutive_days
+        consecutive_days = probability
+        probability = (probability - i)*2+i
+    print(str(no_of_combo_with_three_day_absent+no_of_combo_with_two_day_absent+no_of_combo_with_one_day_absent) + '/' + str(probability))
